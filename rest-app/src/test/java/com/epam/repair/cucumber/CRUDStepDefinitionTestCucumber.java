6fa6d2963f346704bd88675daf09f5d3380c882f
RomanukAlex <Romanuk_Alex@tut.by>
2019-12-17 09:58:48 +0300

    delete - comments, correct empty conditions, add regular expression

--- rest-app/src/test/java/com/epam/repair/cucumber/CRUDStepDefinitionTestCucumber.java
+++ rest-app/src/test/java/com/epam/repair/cucumber/CRUDStepDefinitionTestCucumber.java
@@ -20,10 +20,6 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CRUDStepDefinitionTestCucumber extends AbstractCucumberSpringConfigurationTest {
-    int ORDER_ID_1000 = 1000;
-    private String FIRST_NAME_MORE_40 = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
-    private String FIRST_NAME_0 = "";
-
     private ResponseEntity<RepairOrder> responseEntity = null;
 
     @Autowired
@@ -32,100 +28,90 @@ public class CRUDStepDefinitionTestCucumber extends AbstractCucumberSpringConfig
     @Autowired
     private ObjectMapper objectMapper;
 
-//---ADD---ADD---ADD---ADD---ADD---ADD---ADD---ADD---ADD---ADD---ADD---ADD---ADD---ADD---ADD---ADD---ADD
-
     @Given("A new client brought a device.")
-    public void a_new_customer_brought_a_device() {
+    public void newCustomerBroughtDevice() throws IOException {
+        responseEntity = restTemplate.
+                postForEntity(URL, repairOrderAddTest(), RepairOrder.class);
     }
 
     @When("Add a new client and device.")
-    public void add_a_new_client_and_device() throws IOException {
-        responseEntity = restTemplate.
-                postForEntity(URL, repairOrderAddTest(), RepairOrder.class);
-
+    public void addNewClientAndDevice() {
         assertEquals(responseEntity.getStatusCode(), HttpStatus.CREATED);
     }
 
     @Then("The order is assigned a unique number.")
-    public void the_order_is_assigned_a_unique_number() {
-
+    public void theOrderIsAssignedUniqueNumber() {
         assertNotNull(responseEntity.getBody().getRepairOrderId());
     }
 
-    @Then("Write the client first name more than 40 characters.")
-    public void writeTheCustomerFirstNameMoreThanCharacters() throws IOException {
+    @Then("Write the client first name {string}.")
+    public void writeTheCustomerFirstNameMore40(String FirstNameMore40) throws IOException {
         RepairOrder repairOrderAddValidMore40 = repairOrderAddTest();
-        repairOrderAddValidMore40.getClient().setFirstName(FIRST_NAME_MORE_40);
+        repairOrderAddValidMore40.getClient().setFirstName(FirstNameMore40);
 
         assertThrows(HttpClientErrorException.class, () -> restTemplate.
                 postForEntity(URL, repairOrderAddValidMore40, RepairOrder.class));
     }
 
-    @Then("Write the customer first name than 0 characters.")
-    public void writeTheCustomerFirstNameThan0Characters() throws IOException {
+    @Then("Write the customer first name {string}.")
+    public void writeTheCustomerFirstName0(String FirstName0) throws IOException {
         RepairOrder repairOrderAddValidThan0 = repairOrderAddTest();
-        repairOrderAddValidThan0.getClient().setFirstName(FIRST_NAME_0);
+        repairOrderAddValidThan0.getClient().setFirstName(FirstName0);
 
         assertThrows(HttpClientErrorException.class, () -> restTemplate.
                 postForEntity(URL, repairOrderAddValidThan0, RepairOrder.class));
     }
 
-    @Then("Save empty body.")
-    public void saveEmptyBody() throws IOException {
+    @Then("Save empty body - null.")
+    public void saveEmptyBodyNull() {
         assertThrows(HttpClientErrorException.class, () -> restTemplate.
                 postForEntity(URL, null, RepairOrder.class));
     }
 
-//---DELETE---DELETE---DELETE---DELETE---DELETE---DELETE---DELETE---DELETE---DELETE---DELETE
-
     @Given("User wants to delete order.")
-    public void userWantsToDeleteOrder() {
-    }
-
-    @When("Find the required order.")
-    public void findTheRequiredOrder() throws IOException {
+    public void userWantsToDeleteOrder() throws IOException {
         responseEntity = restTemplate.
                 postForEntity(URL, repairOrderAddTest(), RepairOrder.class);
+    }
+
+    @When("Find the required order. Delete.")
+    public void findTheRequiredOrder() {
+        restTemplate.
+                delete(URL + "/" + responseEntity.getBody().getRepairOrderId());
     }
 
     @Then("The delete 'IS SUCCESSFUL'")
     public void theDeleteIsSuccessful() {
-
-        int orderDeleteId = responseEntity.getBody().getRepairOrderId();
-        restTemplate.
-                delete(URL + "/" + orderDeleteId);
-
         assertThrows(RuntimeException.class, () -> restTemplate.
-                getForEntity(URL + "/" + orderDeleteId, RepairOrder.class));
+                getForEntity(URL + "/" + responseEntity.getBody().getRepairOrderId(), RepairOrder.class));
     }
 
-    @Then("But it is not in the database. The delete 'IS UNSUCCESSFUL'")
-    public void theDeleteIsUnsuccessful() {
+    @Then("But ID = {int} no in the database. The delete 'IS UNSUCCESSFUL'")
+    public void butIDNoInTheDatabaseTheDeleteISUNSUCCESSFUL(int id1000) {
         assertThrows(HttpClientErrorException.class, () -> restTemplate.
-                delete(URL + "/" + ORDER_ID_1000));
+                delete(URL + "/" + id1000));
     }
-
-//---EDIT---EDIT---EDIT---EDIT---EDIT---EDIT---EDIT---EDIT---EDIT---EDIT---EDIT---EDIT---EDIT
 
     @Given("Order is ready to issue.")
-    public void order_is_ready_to_issue() {
+    public void orderIsReadyToIssue() throws IOException {
+        responseEntity = restTemplate.
+                postForEntity(URL, repairOrderAddTest(), RepairOrder.class);
     }
 
     @Given("User need to edit the client first name in a order.")
-    public void userNeedToEditTheClientFirstNameInAOrder() {
+    public void userNeedToEditTheClientFirstNameInAOrder() throws IOException {
+        responseEntity = restTemplate.
+                postForEntity(URL, repairOrderAddTest(), RepairOrder.class);
     }
 
     @When("Find the order.")
-    public void find_the_order() throws IOException {
-        responseEntity = restTemplate.
-                postForEntity(URL, repairOrderAddTest(), RepairOrder.class);
-
+    public void findTheOrder() throws IOException {
         responseEntity = restTemplate.
                 getForEntity(URL + "/" + responseEntity.getBody().getRepairOrderId(), RepairOrder.class);
     }
 
     @Then("Change status --> {string}.")
-    public void change_status_in_work(String expectedStatus) {
+    public void changeStatusInWork(String expectedStatus) {
         responseEntity.getBody().getStatus().setStatusId(3);
 
         restTemplate.put(URL, responseEntity.getBody());
@@ -135,8 +121,6 @@ public class CRUDStepDefinitionTestCucumber extends AbstractCucumberSpringConfig
 
         assertEquals(responseEntity.getBody().getStatus().getStatusName(), expectedStatus);
     }
-
-//---GET---GET---GET---GET---GET---GET---GET---GET---GET---GET---GET---GET---GET---GET---GET---GET
 
     @Given("User wants to get order")
     public void userWantsToGetOrder() throws IOException {
@@ -155,16 +139,14 @@ public class CRUDStepDefinitionTestCucumber extends AbstractCucumberSpringConfig
         assertEquals(addOrderId, getById);
     }
 
-    @Then("But it is not in the database. The get by id 'IS UNSUCCESSFUL'")
-    public void butItIsNotInTheDatabaseTheGetByIdISUNSUCCESSFUL() {
+    @Then("But ID = {int} no in the database. The get by id 'IS UNSUCCESSFUL'")
+    public void butIDNoInTheDatabaseTheGetByIdISUNSUCCESSFUL(int id1000) {
         assertThrows(HttpClientErrorException.class, () -> restTemplate.
-                getForEntity(URL + "/" + ORDER_ID_1000, RepairOrder.class));
+                getForEntity(URL + "/" + id1000, RepairOrder.class));
     }
 
     private RepairOrder repairOrderAddTest() throws IOException {
         return repairOrderService
                 .add(objectMapper.readValue(loadTestFile("json/newOrder.json"), RepairOrder.class));
     }
-
-
 }
--- rest-app/src/test/resources/features/addOrder.feature
+++ rest-app/src/test/resources/features/addOrder.feature
@@ -8,14 +8,14 @@ Feature: Add order
   Scenario: Validation. UNSUCCESSFULLY adding a new order
     Given A new client brought a device.
     When Add a new client and device.
-    Then Write the client first name more than 40 characters.
+    Then Write the client first name "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA".
 
   Scenario: Validation. UNSUCCESSFULLY adding a new order
     Given A new client brought a device.
     When Add a new client and device.
-    Then Write the customer first name than 0 characters.
+    Then Write the customer first name "".
 
   Scenario: Validation. UNSUCCESSFULLY adding a new order
     Given A new client brought a device.
     When Add a new client and device.
-    Then Save empty body.
+    Then Save empty body - null.
