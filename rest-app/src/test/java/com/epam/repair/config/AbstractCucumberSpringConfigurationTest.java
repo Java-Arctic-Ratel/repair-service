de809e5580fae8e00286a8a6c22eee226fa840fc
RomanukAlex <Romanuk_Alex@tut.by>
2019-12-17 17:09:51 +0300

    GLOBAL CORS - correct allowedMethods, test separation --> Add, Delete, Get and Update, create testConfig, moving URL to application.properties

--- rest-app/src/test/java/com/epam/repair/config/AbstractCucumberSpringConfigurationTest.java
+++ rest-app/src/test/java/com/epam/repair/config/AbstractCucumberSpringConfigurationTest.java
@@ -3,12 +3,11 @@ package com.epam.repair.config;
 import com.epam.repair.rest.RestApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest(classes = RestApplication.class,
         webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @ActiveProfiles("dev")
+@ContextConfiguration(classes = TestConfig.class)
 public class AbstractCucumberSpringConfigurationTest {
-    protected static final String URL = "http://localhost:8090/order";
-    protected RestTemplate restTemplate = new RestTemplate();
 }
